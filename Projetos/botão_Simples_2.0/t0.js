2745f58667e1dcd74acf2c0333580dd899035172
Thom <thomazerick35@gmail.com>
2025-08-27 14:23:19 -0300

    a

---
+++ Projetos/botão_Simples_2.0/t0.js
@@ -0,0 +1,16 @@
+var cad = document.getSelection('div#b1')
+var login = document.getElementById('b2')
+var int = document.getElementById('b3')
+
+function clicar() {
+    var cad1 = prompt('Cadastre seu email:')
+    
+}
+
+function clicar1() {
+    
+}
+
+function clicar2() {
+    
+}
